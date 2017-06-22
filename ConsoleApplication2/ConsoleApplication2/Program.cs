d3163839424be7dff7a8482d9e68ddaaea9bb159
Peter Jongste <peter.jongste@alten.nl>
2017-06-22 11:55:04 +0200

    Initial commit.

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (1 == 2)
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# PR18880
+Test project for TIOBE PR18880 (see http://portal.tiobe.com/bugtracker?id:18880).
+TICS cannot analyze this project correctly due to the relative path to the keyfile.
